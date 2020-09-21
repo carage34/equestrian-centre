98709c1665cf18c8fe48eac900ffc2aeb90bf7f7
morgan <jully.morgan@orange.fr>
2020-09-21 17:55:44 +0200

    nodejs backend added

---
+++ backend/server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const app = express();
+
+var corsParam = {
+    origin: "http://localhost:4200"
+};
+
+app.use(cors(corsParam));
+
+app.use(bodyParser.json());
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", function(req, res) {
+    res.json("Api status: Ok");
+})
+
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, function() {
+    console.log("Server is listening on port " + PORT);
+})
